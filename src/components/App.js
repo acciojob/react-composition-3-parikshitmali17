9d0ad6961db1289d644c07c5251b85cbaaf42cef
Parikshit <Parikshitmali17@gmail.com>
2025-04-25 15:52:55 +0530

    d3

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,48 @@
 
-import React,{useState} from "react";
-import './../styles/App.css';
-import { Tooltip } from "./Tooltip";
+// import React,{useState} from "react";
+// import './../styles/App.css';
+// import { Tooltip } from "./Tooltip";
 
-const App = () => {
+// const App = () => {
 
-  const [text1,setText1]=useState("");
-  const [text2,setText2]=useState("");
+//   const [text1,setText1]=useState("");
+//   const [text2,setText2]=useState("");
 
- function handleOver1(){
-setText1("This is a tooltip")
- }
+//  function handleOver1(){
+// setText1("This is a tooltip")
+//  }
 
- function handleOver2(){
-  setText2("This is another tooltip")
+//  function handleOver2(){
+//   setText2("This is another tooltip")
   
- }
+//  }
 
 
-  return (
-    <div>
-      <Tooltip  text={text1} />
-     <h1 style={{height:"200px" ,width:"400px"}} onMouseOut={()=>setText1("")} onMouseOver={handleOver1} >Hover Over Me</h1>
+//   return (
+//     <div>
+//       <Tooltip  text={text1} />
+//      <h1 style={{height:"200px" ,width:"400px"}} onMouseEnter={()=>setText1("")} onMouseLeave={handleOver1} >Hover Over Me</h1>
 
-     <h4 onMouseOut={()=>setText2("")}  onMouseOver={handleOver2}>This is another Hover</h4>
+//      <h4 onMouseOut={()=>setText2("")}  onMouseOver={handleOver2}>This is another Hover</h4>
      
-     <Tooltip text={text2}/>
+//      <Tooltip text={text2}/>
+//     </div>
+//   )
+// }
+
+// export default App
+import React, { useState } from 'react'
+import Tooltip from './Tooltip'
+
+
+export const App = () => {
+ 
+  return (
+
+    <div>
+      <Tooltip text="This is a Tooltip"><h2>Hover over me</h2>
+      </Tooltip>
+      <Tooltip text="This is another Tooltip"><h4>Hover over this Paragraph</h4></Tooltip>
     </div>
   )
 }
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,7 +1,23 @@
-import React from 'react'
+// import React from 'react'
 
-export const Tooltip = (props) => {
+// export const Tooltip = (props) => {
+//   return (
+//     <div className='tooltip'><p className='tooltiptext' style={{backgroundColor: props.text.length ==0 ? "white" :"red", fontSize:"30px", color:'white', display:"inline-block", height:"100px", width:"200px",display:"flex",  padding:"20px", alignItems:'center'}} >{props.text}</p></div>
+//   )
+// }
+
+import React, { useState } from 'react'
+
+export const Tooltip = ({text, children}) => {
+
+  const [visible, setvisible]=useState(false)
+
   return (
-    <div className='tooltip'><p className='tooltiptext' style={{backgroundColor: props.text.length ==0 ? "white" :"red", fontSize:"30px", color:'white', display:"inline-block", height:"100px", width:"200px",display:"flex",  padding:"20px", alignItems:'center'}} >{props.text}</p></div>
+    <div className='tooltip' onMouseLeave={()=>{setvisible(false)}} onMouseEnter={()=>{setvisible(true)}}>
+      {children}
+      {visible && <div className='tooltiptext' >{text}</div>}
+    </div>
   )
 }
+
+export default Tooltip
